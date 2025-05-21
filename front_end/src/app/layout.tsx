b9bc9d430d62ab54086cf0b88f2e5aee3ef17dbd
JayanAXHF <sunil.chdry@gmail.com>
2025-05-22 00:28:50 +0530

    Added app logo

--- front_end/src/app/layout.tsx
+++ front_end/src/app/layout.tsx
@@ -7,10 +7,10 @@ import { ThemeProvider } from "../components/theme-provider";
 import { PostHogProvider } from "../components/PostHogProvider";
 
 export const metadata: Metadata = {
-  title: "Homeworker",
+  title: "Valtam",
   description:
     "This is the interschool code for the Shiv Nadar AI Chatbot competition from the Lotus Valley Gurgaon Team. It is an AI chatbot which runs on NextJS. The llm was built in python, and we use fastapi for the backend to interact with the LLM. ",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  icons: [{ rel: "icon", url: "/favicon.png" }],
 };
 
 const font = Roboto({
@@ -37,3 +37,4 @@ export default function RootLayout({
     </html>
   );
 }
+
